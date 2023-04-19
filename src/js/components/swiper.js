04357778975ac4241c9cd3641b98deee761fa84b
Dmitry Greshnov <nastybard19@mail.ru>
2023-04-19 15:31:32 +0300

    feat: add adaptive and burger-menu

--- src/js/components/swiper.js
+++ src/js/components/swiper.js
@@ -1,18 +1,25 @@
-import Swiper, { Navigation, Pagination, Autoplay } from 'swiper';
-Swiper.use([Navigation, Pagination, Autoplay]);
+import Swiper, { Navigation, Pagination, Autoplay, EffectFlip, EffectFade } from 'swiper';
+Swiper.use([Navigation, Pagination, Autoplay, EffectFlip, EffectFade]);
 
 const swiper = new Swiper('.how__swiper', {
+  // effect: 'flip',
+  // flipEffect: {
+  //   slideShadows: false,
+  // },
+  effect: 'fade',
+  speed: 3000,
   slidesPerView: 1,
-  loop: true,
   autoplay: {
-    delay: 5000,
-    disableOnInteraction: false,
+    delay: 10000,
   },
 });
 
 const swiper2 = new Swiper('.reviews__swiper', {
   slidesPerView: 1,
   loop: true,
+  // autoplay: {
+  //   delay: 5000,
+  // },
   pagination: {
     el: '.swiper-pagination',
     type: 'bullets',
